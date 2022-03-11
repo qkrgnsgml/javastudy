5e0cbc46debf8283c07481e718a986efe69b31ec
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-03-12 01:47:13 +0900

    2022-03-12

---
+++ src/codingtest/FirstLevel6.java
@@ -0,0 +1,23 @@
+package codingtest;
+
+public class FirstLevel6 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int i =0;
+        int sum=0;
+        for (boolean sign : signs) {
+            if(!sign){
+                absolutes[i] *=-1;
+            }
+            sum+=absolutes[i];
+            i++;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        FirstLevel6 firstLevel6 = new FirstLevel6();
+        int[] a = {4,7,12};
+        boolean[] b = {true,false,true};
+        System.out.println(firstLevel6.solution(a,b));
+    }
+}
