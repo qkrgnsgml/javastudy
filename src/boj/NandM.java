3e20e884254c58cfb80929e216cac2aa81a79d99
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-03-19 22:28:56 +0900

    2022-03-19

---
+++ src/boj/NandM.java
@@ -0,0 +1,20 @@
+package boj;
+
+public class NandM {
+
+    static StringBuilder sb = new StringBuilder();
+
+    static int N,M;
+    static int[] selected;
+
+    private static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+
+    }
+
+    public static void main(String[] args) {
+        input();
+    }
+}
---
+++ src/codingtest/FirstLevel14.java
@@ -0,0 +1,49 @@
+package codingtest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstLevel14 {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        Map<Integer, Integer> has = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            has.put(i, 0);
+        }
+        for (int i : lost) {
+            has.put(i, has.get(i) - 1);
+        }
+        for (int i : reserve) {
+            has.put(i, has.get(i) + 1);
+        }
+
+        for (Integer integer : has.keySet()) {
+            if (has.get(integer) == -1) {
+                if (integer!=1&&has.get(integer - 1) == 1) {
+                    has.put(integer, has.get(integer) + 1);
+                    has.put(integer - 1, has.get(integer - 1) - 1);
+                    continue;
+                } else if (integer!=n&&has.get(integer + 1) == 1) {
+                    has.put(integer, has.get(integer) + 1);
+                    has.put(integer + 1, has.get(integer + 1) - 1);
+                    continue;
+                }
+            }
+        }
+        for (Integer value : has.values()) {
+            if (value >= 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FirstLevel14 firstLevel14 = new FirstLevel14();
+        int a = 5;
+        int[] b = {2,4};
+        int[] c = {1, 3, 5};
+        System.out.println(firstLevel14.solution(a,b,c));
+    }
+}
