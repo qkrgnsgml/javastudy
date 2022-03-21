eac072e1ec3573dca9d282d1804e90c97773d3b1
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-03-22 01:24:43 +0900

    2022-03-21

--- src/boj/NandM.java
+++ src/boj/NandM.java
@@ -7,6 +7,19 @@ public class NandM {
     static int N,M;
     static int[] selected;
 
+    static void rec_func(int k){
+        if(k==M+1){
+            for (int i = 1; i <= M; i++) sb.append(selected[i]).append(' ');
+            sb.append('\n');
+        }else{
+            for(int cand = 1;cand<=N; cand++){
+                selected[k] = cand;
+                rec_func(k+1);
+                selected[k]=0;
+            }
+        }
+    }
+
     private static void input() {
         FastReader scan = new FastReader();
         N = scan.nextInt();
@@ -16,5 +29,7 @@ public class NandM {
 
     public static void main(String[] args) {
         input();
+
+        rec_func(1);
     }
 }
---
+++ src/codingtest/FirstLevel16.java
@@ -0,0 +1,57 @@
+package codingtest;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FirstLevel16 {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        int fail = 0;
+        int cnt = 0;
+        float failRate = 0.0F;
+        Map<Integer, Float> numCnt = new LinkedHashMap<>();
+
+        for (int i = 1; i <= N; i++) {
+            fail = 0;
+            cnt = 0;
+            for (int j = 0; j < stages.length; j++) {
+                if (i == stages[j]) {
+                    fail++;
+                }
+                if (stages[j] >= i) {
+                    cnt++;
+                }
+            }
+            if (cnt == 0) {
+                failRate = 0.0F;
+            } else {
+                failRate = (float) fail / cnt;
+            }
+            numCnt.put(i, failRate);
+        }
+
+        List<Float> floats = numCnt.values().stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+
+        int i = 0;
+        for (Float aFloat : floats) {
+            for (Integer integer : numCnt.keySet()) {
+                if (aFloat == numCnt.get(integer)) {
+                    answer[i] = integer;
+                    break;
+                }
+            }
+            i++;
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int[] b = {2, 1, 2, 4, 2, 4, 3, 3};
+        FirstLevel16 firstLevel16 = new FirstLevel16();
+        System.out.println(firstLevel16.solution(a, b));
+    }
+}
