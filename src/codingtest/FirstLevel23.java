9213327b2b69ccabd78c671be828b48a5c9aea16
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-03-26 21:56:03 +0900

    2022-03-26

---
+++ src/codingtest/FirstLevel23.java
@@ -0,0 +1,17 @@
+package codingtest;
+
+public class FirstLevel23 {
+    public long solution(int price, int money, int count) {
+        for(int i=1;i<=count;i++){
+            money-=(price*i);
+        }
+
+        if(money>=0){
+            return 0;
+        }else{
+            return -(money);
+        }
+
+
+    }
+}
