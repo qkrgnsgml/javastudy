809c7b485d835d8e0a16d5a78b2b6bf9da4d94c2
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-05-03 23:30:28 +0900

    2022-05-03

---
+++ src/codingtest/secondlevel/SecondLevel12.java
@@ -0,0 +1,29 @@
+package codingtest.secondlevel;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SecondLevel12 {
+    public String solution(String number, int k) {
+        StringBuilder a = new StringBuilder(number);
+        int i = 0;
+        int j = 0;
+        int idx;
+        int l;
+
+        for (i = 0; i < k; i++) {
+            l=a.length();
+            idx = l - 1;
+            for (j = 0; j < l - 1; j++) {
+                if (a.charAt(j) < a.charAt(j + 1)) {
+                    idx = j;
+                    break;
+                }
+            }
+
+            a.deleteCharAt(idx);
+        }
+
+        return a.toString();
+    }
+}
